3169a00345491ad7b9ecb9c13d04696deccb93bc
Jebakani <vjishwarya@gmail.com>
2021-08-27 08:53:54 +0530

    [JEBAKANI]Add: Creating the model Repository Controller and Manager folders for fundoo notes

---
+++ Model/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundooNotes1.Model
+{
+    public class RegisterModel
+    {
+        [key]
+        public string id { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
